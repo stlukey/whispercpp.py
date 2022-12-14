a021ae79daa8fc0a95d125386a378796cf7f6d2b
Luke Southam <luke@devthe.com>
2022-12-14 16:42:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from Cython.Build import cythonize
 import numpy, os, sys
 
 if sys.platform == 'darwin':
-    os.environ['CFLAGS']   = '-DGGML_USE_ACCELERATE -O3 -I./whisper.cpp/'
-    os.environ['CXXFLAGS'] = '-DGGML_USE_ACCELERATE -O3 -I./whisper.cpp/'
-    os.environ['LDFLAGS']  = '-framework Accelerate -I./whisper.cpp/'
+    os.environ['CFLAGS']   = '-DGGML_USE_ACCELERATE -O3'
+    os.environ['CXXFLAGS'] = '-DGGML_USE_ACCELERATE -O3'
+    os.environ['LDFLAGS']  = '-framework Accelerate'
 else:
-    os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3 -I./whisper.cpp/'
-    os.environ['CXXFLAGS'] = '-mavx -mavx2 -mfma -mf16c -O3 -I./whisper.cpp/'
+    os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3'
+    os.environ['CXXFLAGS'] = '-mavx -mavx2 -mfma -mf16c -O3'
 
 
 setup(
